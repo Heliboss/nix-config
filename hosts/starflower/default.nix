0a4395c5913a0812ada7a3bf7a3adfe75399f837
Heliboss <99956405+Heliboss@users.noreply.github.com>
2025-05-21 08:52:07 +0800

    Switch to zram with loop device as writeback

--- hosts/starflower/default.nix
+++ hosts/starflower/default.nix
@@ -28,16 +28,48 @@
 
   networking.hostName = "starflower";
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "zram" ];
+  };
 
   swapDevices = [{
     device = "/swap/swapfile";
     size = 7311;
   }];
 
-  boot.kernelParams = [
-    "zswap.enabled=1"
-  ];
+  systemd.services.zramloop = {
+    description = "Attach /swap/swapfile to /dev/loop0 for zram writeback";
+    wantedBy = [ "multi-user.target" ];
+    path = [ "/run/current-system/sw" ];
+    script = ''
+      if swapoff /dev/zram0; then
+        echo swapoff
+      fi
+      sleep 1
+      if losetup -d /dev/loop0; then
+        echo detach
+      fi
+      sleep 1
+      if losetup /dev/loop0 /swap/swapfile; then
+        echo loop
+      fi
+      if rmmod zram; then
+        echo unload
+      fi
+      sleep 1
+      if modprobe zram; then
+        echo load
+      fi
+      echo zstd > /sys/block/zram0/comp_algorithm
+      echo /dev/loop0 > /sys/block/zram0/backing_dev
+      echo 7311M > /sys/block/zram0/disksize
+      sleep 1
+      mkswap -U clear /dev/zram0
+      swapon -d -p 100 /dev/zram0
+    '';
+    serviceConfig.DeviceAllow = [ "/dev/loop-control" "/dev/loop0" "/dev/zram0" ];
+  };
 
   boot.kernel.sysctl = {
     "vm.swappiness" = 10;
