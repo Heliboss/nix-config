a59424ca9d86bfee3d2b5123d7ea7d31ae722bc6
Heliboss <99956405+Heliboss@users.noreply.github.com>
2025-06-19 23:33:29 +0800

    feat: blank firefox startup page

--- users/nyaur/programs/graphical/firefox/user-overrides.js
+++ users/nyaur/programs/graphical/firefox/user-overrides.js
@@ -1,4 +1,4 @@
 user_pref("browser.startup.page", 1);
-user_pref("browser.startup.homepage", "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&hyl_auth_required=true&hyl_presentation_style=fullscreen&utm_source=hoyolab&utm_medium=tools&lang=en-us&bbs_theme=light&bbs_theme_device=1");
+user_pref("browser.startup.homepage", "moz-extension://4ebca9d9-1a53-476d-b203-e0554014a5f9/static/newtab.html");
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("media.eme.enabled", true);
