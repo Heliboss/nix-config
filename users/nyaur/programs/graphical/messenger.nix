7f35d127e8614a3e9b9690c3a187a159fc78b201
Heliboss <99956405+Heliboss@users.noreply.github.com>
2025-08-26 12:37:00 +0800

    feat: use own fork of caprine

--- users/nyaur/programs/graphical/messenger.nix
+++ users/nyaur/programs/graphical/messenger.nix
@@ -1,9 +1,19 @@
 {
+  lib,
   pkgs,
   ...
-}: {
-  home.packages = with pkgs; [
-    caprine
+}: let
+  caprine-override = pkgs.caprine.overrideAttrs (old: rec {
+    src = pkgs.fetchFromGitHub {
+      owner = "heliboss";
+      repo = "caprine";
+      rev = "fb5c769";
+      hash = "sha256-G77Rmo43KJWxt/mN9/hWNfCPAmBEsL9w65i40XuS0PU=";
+    };
+  });
+in {
+  home.packages = [
+    caprine-override
   ];
 
   home.persistence."/persist/home/nyaur" = {
