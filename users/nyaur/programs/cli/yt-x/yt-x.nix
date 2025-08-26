fbb2602a4c3deb4c4f013a910106a6184f6756ef
Heliboss <99956405+Heliboss@users.noreply.github.com>
2025-08-26 12:32:19 +0800

    fix: correct home.file directory

--- users/nyaur/programs/cli/yt-x/yt-x.nix
+++ users/nyaur/programs/cli/yt-x/yt-x.nix
@@ -3,6 +3,6 @@
   ...
 }: {
   home.file = {
-    ".config/yt-x/yt-x.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/yt-x/yt-x.conf";
+    ".config/yt-x/yt-x.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/yt-x/yt-x/yt-x.conf";
   };
 }
