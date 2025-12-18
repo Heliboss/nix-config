fdc46d10675a8803448a6b6986eda07f638e76fb
Heliboss <99956405+Heliboss@users.noreply.github.com>
2025-12-19 00:23:21 +0800

    feat: switch to zen kernel and add zram writeback

--- hosts/starflower/default.nix
+++ hosts/starflower/default.nix
@@ -24,12 +24,53 @@
 
   networking.hostName = "starflower";
 
-  boot = { kernelPackages = pkgs.linuxPackages_latest; };
-
-  zramSwap = {
-    enable = true;
-    memoryPercent = 200;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    kernelModules = [ "zram" ];
   };
+
+  swapDevices = [{
+    device = "/tmp/swapfile";
+    size = 7311;
+  }];
+
+  systemd.services.zramloop = {
+    description = "Attach /swap/swapfile to /dev/loop0 for zram writeback";
+    wantedBy = [ "multi-user.target" ];
+    path = [ "/run/current-system/sw" ];
+    script = ''
+      if swapoff /dev/zram0; then
+        echo swapoff
+      fi
+      sleep 1
+      if rmmod zram; then
+        echo unload
+      fi
+      sleep 1
+      if modprobe zram; then
+        echo load
+      fi
+      sleep 1
+      if losetup -d /dev/loop0; then
+        echo detach
+      fi
+      sleep 1
+      if losetup /dev/loop0 /tmp/swapfile; then
+        echo loop
+      fi
+      sleep 1
+      echo zstd > /sys/block/zram0/comp_algorithm
+      echo /dev/loop0 > /sys/block/zram0/backing_dev
+      echo 7311M > /sys/block/zram0/disksize
+      sleep 1
+      mkswap -U clear /dev/zram0
+      swapon -d -p 100 /dev/zram0
+    '';
+    serviceConfig.DeviceAllow =
+      [ "/dev/loop-control" "/dev/loop0" "/dev/zram0" ];
+  };
+
+  boot.kernel.sysctl = { "vm.swappiness" = 10; };
 
   xdg.portal = {
     extraPortals = config.home-manager.users.nyaur.xdg.portal.extraPortals;
