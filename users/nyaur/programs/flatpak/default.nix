a11759a153291b93d0fd1411f4fd98be07c9aa9f
Heliboss <99956405+Heliboss@users.noreply.github.com>
2025-12-19 00:23:53 +0800

    feat: add soundux

--- users/nyaur/programs/flatpak/default.nix
+++ users/nyaur/programs/flatpak/default.nix
@@ -1,20 +1,16 @@
-{
-  inputs,
-  ...
-}: {
-  imports = [
-    inputs.nix-flatpak.homeManagerModules.nix-flatpak
-  ];
+{ inputs, ... }: {
+  imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
 
   home.persistence."/persist/home/nyaur" = {
-    directories = [
-      ".local/share/flatpak"
-      ".var/app"
-    ];
+    directories = [ ".local/share/flatpak" ".var/app" ];
   };
 
-  services.flatpak.packages = [
-    "org.vinegarhq.Sober"
-    "net.davidotek.pupgui2"
-  ];
+  services.flatpak = {
+    packages =
+      [ "org.vinegarhq.Sober" "net.davidotek.pupgui2" "io.github.Soundux" ];
+    overrides = {
+      "io.github.Soundux".Context.filesystems =
+        [ "/persist/home/nyaur/Music/soundux:create" ];
+    };
+  };
 }
