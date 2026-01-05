19e7f426e00d9841094470c41c26f581dc4272e7
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-01-05 09:22:29 +0800

    chore: update easyeffects presets

--- users/nyaur/programs/graphical/easyeffects/default.nix
+++ users/nyaur/programs/graphical/easyeffects/default.nix
@@ -1,12 +1,15 @@
-{
-  config,
-  ...
-}: {
+{ config, ... }: {
   services.easyeffects.enable = true;
 
   home.file = {
-    ".config/easyeffects/autoload".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/autoload";
-    ".config/easyeffects/input".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/input";
-    ".config/easyeffects/output".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/output";
+    ".local/share/easyeffects/autoload".source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/autoload";
+    ".local/share/easyeffects/input".source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/input";
+    ".local/share/easyeffects/output".source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/output";
   };
 }
