20b3ea347078f4c366343c88f9d8745927c6968d
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-01-23 11:45:06 +0800

    feat: add blender

--- users/nyaur/programs/creativity/default.nix
+++ users/nyaur/programs/creativity/default.nix
@@ -1,23 +1,14 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  imports = [
-    ./obs.nix
-    ./krita
-    ./gimp.nix
-  ];
+{ config, pkgs, ... }: {
+  imports = [ ./obs.nix ./krita ./gimp.nix ./blender.nix ];
 
-  home.packages = with pkgs; [
-    kdePackages.kdenlive
-    reaper
-    ffmpeg-full
-    yt-dlp
-  ];
+  home.packages = with pkgs; [ kdePackages.kdenlive reaper ffmpeg-full yt-dlp ];
 
   home.file = {
-    ".config/OpenTabletDriver/Presets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/creativity/OpenTabletDriver/Presets";
-    ".config/OpenTabletDriver/Plugins".source = config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/creativity/OpenTabletDriver/Plugins";
+    ".config/OpenTabletDriver/Presets".source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/creativity/OpenTabletDriver/Presets";
+    ".config/OpenTabletDriver/Plugins".source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/creativity/OpenTabletDriver/Plugins";
   };
 }
