8ff919f1825ae911308cee6c2824e8ab075c9123
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-02-24 04:12:39 +0800

    feat: fix pavucontrol broken theming

--- users/nyaur/programs/graphical/pavucontrol.nix
+++ users/nyaur/programs/graphical/pavucontrol.nix
@@ -4,4 +4,11 @@
   home.persistence."/persist" = {
     directories = [ ".local/state/wireplumber" ];
   };
+
+  xdg.desktopEntries = {
+    "org.pulseaudio.pavucontrol" = {
+      name = "Volume Control";
+      exec = "env GTK_THEME=Adwaita-dark pavucontrol %U";
+    };
+  };
 }
