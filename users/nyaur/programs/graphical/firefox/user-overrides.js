37883883536520ac26ad09ed1ffa2ffb49df521c
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-03-20 11:50:47 +0800

    chore: update firefox config

--- users/nyaur/programs/graphical/firefox/user-overrides.js
+++ users/nyaur/programs/graphical/firefox/user-overrides.js
@@ -1,5 +1,12 @@
 user_pref("browser.startup.page", 1);
-user_pref( "browser.startup.homepage", "moz-extension://4ebca9d9-1a53-476d-b203-e0554014a5f9/static/newtab.html");
+user_pref("browser.startup.homepage", "moz-extension://4ebca9d9-1a53-476d-b203-e0554014a5f9/static/newtab.html");
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("media.eme.enabled", true);
 user_pref("layout.css.font-visibility", 1);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearOnShutdown_v2.downloads", true);
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", true);
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", true);
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", true);
