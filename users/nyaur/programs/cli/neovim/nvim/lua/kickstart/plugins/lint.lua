720033fa91d2b6b367e6255dbc8a515a6153a4e9
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-03-20 11:51:58 +0800

    chore: update neovim config

--- users/nyaur/programs/cli/neovim/nvim/lua/kickstart/plugins/lint.lua
+++ users/nyaur/programs/cli/neovim/nvim/lua/kickstart/plugins/lint.lua
@@ -6,6 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft['json'] = nil
 
       -- However, note that this will enable a set of default linters,
       -- which will cause errors unless these tools are available:
