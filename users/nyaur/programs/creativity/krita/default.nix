75f78dbd818b058fbb4a1d014c73a4ef9ee00413
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-05-23 00:08:47 +0800

    fix: use symlink for krita impermanence

--- users/nyaur/programs/creativity/krita/default.nix
+++ users/nyaur/programs/creativity/krita/default.nix
@@ -1,14 +1,24 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   home.packages = with pkgs; [ krita ];
 
   home.persistence."/persist" = {
     directories = [ ".local/share/krita" ];
-    files = [ ".config/kritarc" ".config/kritadisplayrc" ];
+    files = [
+      {
+        file = ".config/kritarc";
+        method = "symlink";
+      }
+      {
+        file = ".config/kritadisplayrc";
+        method = "symlink";
+      }
+    ];
   };
 
   home.file = {
-    ".config/kritashortcutsrc".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/creativity/krita/kritashortcutsrc";
+    ".config/kritashortcutsrc".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/creativity/krita/kritashortcutsrc";
 
   };
 }
