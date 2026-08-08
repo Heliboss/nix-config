dba72733df57e3f46dc92edc64a2febd4e50ef28
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-08-09 00:29:20 +0800

    style: added stylua config

--- users/nyaur/programs/graphical/hyprland/hypr/conf/monitors.lua
+++ users/nyaur/programs/graphical/hyprland/hypr/conf/monitors.lua
@@ -1,2 +1,2 @@
 -- Default monitor settings
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor { output = '', mode = 'preferred', position = 'auto', scale = 1 }
