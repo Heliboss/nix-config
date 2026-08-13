9d5baea9c5a5071cda2859addd9e1d84848d7539
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-08-13 10:15:54 +0800

    chore: add impermanence for .cache/fastfetch

--- users/nyaur/programs/cli/fastfetch/default.nix
+++ users/nyaur/programs/cli/fastfetch/default.nix
@@ -6,4 +6,10 @@
     ".config/fastfetch".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/fastfetch/fastfetch";
   };
+
+  home.persistence."/persist" = {
+    directories = [
+      ".cache/fastfetch"
+    ];
+  };
 }
