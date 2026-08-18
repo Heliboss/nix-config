d06fa323f3f79e5fae4653577e38aca0539a474f
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-08-19 01:27:36 +0800

    chore(vale): add 'enumerate' to accepted vocab

--- users/nyaur/programs/cli/neovim/vale/default.nix
+++ users/nyaur/programs/cli/neovim/vale/default.nix
@@ -1,10 +1,18 @@
-{ config, pkgs, ... }: {
-  home.packages = with pkgs; [ vale vale-ls ];
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    vale
+    vale-ls
+  ];
 
   home.file = {
-    ".config/vale/.vale.ini".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/neovim/vale/.vale.ini";
+    ".config/vale/.vale.ini".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/neovim/vale/.vale.ini";
+    ".local/share/vale/styles/config/vocabularies/custom/accept.txt".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/neovim/vale/accept.txt";
   };
 
-  home.persistence."/persist" = { directories = [ ".local/share/vale" ]; };
+  home.persistence."/persist" = {
+    directories = [ ".local/share/vale" ];
+  };
 }
