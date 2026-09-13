736142e837290b3231a58581703ce1b15d18c816
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-09-13 13:36:50 +0800

    feat(yazi): switch to rose-pine-moon flavor

--- users/nyaur/programs/cli/yazi/default.nix
+++ users/nyaur/programs/cli/yazi/default.nix
@@ -2,13 +2,12 @@
 {
   programs.yazi = {
     enable = true;
-    flavors = {
-      rose-pine = ./rose-pine.yazi;
-    };
   };
 
   home.file = {
     ".config/yazi/theme.toml".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/yazi/theme.toml";
+    ".config/yazi/flavors".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/cli/yazi/flavors";
   };
 }
