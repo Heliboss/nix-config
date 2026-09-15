b2d1c4d4edf3174dec791e56ac43f50ceab8792d
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-09-15 16:45:58 +0800

    feat(easyeffects): add rose-pine-moon theme

--- users/nyaur/programs/graphical/easyeffects/default.nix
+++ users/nyaur/programs/graphical/easyeffects/default.nix
@@ -1,17 +1,17 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   home.packages = with pkgs; [ easyeffects ];
 
   home.file = {
     ".local/share/easyeffects/autoload".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/autoload";
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/autoload";
     ".local/share/easyeffects/input".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/input";
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/input";
     ".local/share/easyeffects/output".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/output";
-    ".config/easyeffectsrc".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/easyeffectsrc";
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/output";
+    ".config/easyeffectsrc".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/easyeffectsrc";
+    ".config/easyeffects/db/graphrc".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/easyeffects/graphrc";
   };
 }
