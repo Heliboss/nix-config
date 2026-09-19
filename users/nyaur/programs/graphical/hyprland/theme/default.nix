502626a367955ccd40d71ce23580750df0bd83a7
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-09-19 15:56:26 +0800

    feat(hypr): add gtk rose-pine dark theme

--- users/nyaur/programs/graphical/hyprland/theme/default.nix
+++ users/nyaur/programs/graphical/hyprland/theme/default.nix
@@ -1,7 +1,18 @@
 { config, ... }:
 {
+  dconf = {
+    enable = true;
+    settings."org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
   home.file = {
     ".local/share/color-schemes".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/hyprland/theme/color-schemes";
+    ".config/gtk-3.0".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/hyprland/theme/gtk-3.0";
+    ".config/gtk-4.0".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/hyprland/theme/gtk-4.0";
   };
 }
