6d807da1a0ff9e1cb7da2a8ca3c1c80a49f1a057
Heliboss <99956405+Heliboss@users.noreply.github.com>
2026-09-19 18:48:44 +0800

    feat(vesktop): add settings

--- users/nyaur/programs/graphical/vesktop/default.nix
+++ users/nyaur/programs/graphical/vesktop/default.nix
@@ -11,5 +11,9 @@
   home.file = {
     ".config/vesktop/themes".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/vesktop/themes";
+    ".config/vesktop/settings.json".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/vesktop/settings.json";
+    ".config/vesktop/settings/settings.json".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.sessionVariables.FLAKE}/users/nyaur/programs/graphical/vesktop/settings/settings.json";
   };
 }
